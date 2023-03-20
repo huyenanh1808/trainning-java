d08b26c8969a89696cc46ed361bc6c656c11a9ce
HUYENANH\admin <chient012@gmail.com>
2023-03-20 22:24:19 +0700

    lab1

---
+++ lab1/src/ClassTest.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ClassTest {
+    public static void main(String[] args) {
+//        int a = 3;
+//        int b = 10;
+//        int c = b % a;
+//        System.out.println("gia tri cua c la : " + c);
+//        System.out.print("||gia tri cua c la : " + c);
+//        System.out.printf("Đào tạo %d nghề %d", 12, 1);
+        Scanner sc = new Scanner(System.in);
+
+        String khaiBaoScanner;
+
+        khaiBaoScanner = sc.nextLine();
+
+        System.out.println("day la chuoi nhap vao tu ban phim : " + khaiBaoScanner);
+    }
+}
